4994dc7d616737a91248b1eb20207942116c9e45
abu al ameen <abulyaqs@gmail.com>
2024-03-15 00:14:01 +0100

    interview on lock box

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    num_boxes = len(boxes)  # Total number of boxes
+    visited = set()  # Set to keep track of visited boxes
+    keys = set([0])
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+        visited.add(current_box)
+        # Collect keys from the current box
+        for key in boxes[current_box]:
+            if key not in keys:
+                keys.add(key)
+                stack.append(key)
+        # Check if all boxes can be opened
+        if len(visited) == num_boxes:
+            return True
+    return False  # If not all boxes can be visited
