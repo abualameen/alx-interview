75e3d529b24a69a8ed71f49b2886cd3091b3e113
abu al ameen <abulyaqs@gmail.com>
2024-05-01 21:09:30 +0100

    interview question

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Making Change."""
 
+
 def makeChange(coins, total):
-    """Calculate the fewest number of coins needed to meet the total."""
+    """Calculate the fewest number of coins
+       needed to meet the total.
+    """
     if total <= 0:
         return 0
 
@@ -14,12 +17,3 @@ def makeChange(coins, total):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
-
-if __name__ == "__main__":
-    coins = [1, 2, 25]
-    total = 37
-    print(makeChange(coins, total))
-
-    coins = [1256, 54, 48, 16, 102]
-    total = 1453
-    print(makeChange(coins, total))
