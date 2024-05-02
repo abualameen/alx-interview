697b3c809e62445bf222a4a82868789a5a8a53f8
abu al ameen <abulyaqs@gmail.com>
2024-05-02 12:16:08 +0100

    improving

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,11 +13,11 @@ def makeChange(coins, total):
     for j in range(1, total + 1):
         for k in range(m):
             if coins[k] <= j:
-                sub_res = table[j -coins[k]]
+                sub_res = table[j - coins[k]]
                 if sub_res != float('inf') and sub_res + 1 < table[j]:
                     table[j] = sub_res + 1
     if table[total] != float('inf'):
         return table[total]
     else:
         return -1
-    return table[total] 
+    return table[total]
