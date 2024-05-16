01da9ed4386925c493f6939b69a35179992ac0f7
abu al ameen <abulyaqs@gmail.com>
2024-05-17 00:30:44 +0100

    now

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,52 @@
 #!/usr/bin/python3
-"""
-Prime Game Module
-"""
-
-def sieve_of_eratosthenes(max_num):
+""" """
+def sieve_of_eratosthenes(n):
     """ """
-    sieve = [True] * (max_num + 1)
-    sieve[0] = sieve[1] = False
+    primes = [True] * (n+1)
+    p = 2
+    while p*p <= n:
+        if primes[p]:
+            for i in range(p*p, n+1, p):
+                primes[i] = False
+        p += 1
+    prime_numbers = [p for p in range(2, n+1) if primes[p]]
+    return prime_numbers
 
-    for start in range(2, int(max_num ** 0.5) + 1):
-        if sieve[start]:
-            for multiple in range(start * start, max_num + 1, start):
-                sieve[multiple] = False
 
-    return [num for num, is_prime in enumerate(sieve) if is_prime]
 
-def find_winner(primes, n):
-    """ """
-    current_set = list(range(1, n + 1))
-    turn = 0
-
-    while True:
-        available_prime = next((p for p in primes if p in current_set), None)
-        if available_prime is None:
-            break
-
-        current_set = [num for num in current_set if num % available_prime != 0]
-        turn = 1 - turn
-
-    return 'Ben' if turn == 1 else 'Maria'
 
 def isWinner(x, nums):
     """ """
-    if not nums or x < 1:
-        return None
+    mariaWins, benWins = 0, 0
+    player_turn = 'Maria'
+    original_set = set(nums)
+    for i in range(x+1):
+      
+        while original_set:
+            if player_turn == 'Maria':
+                prime_numbs = sieve_of_eratosthenes(max(original_set))
+                player_input = int(input(f"{player_turn}'s turn. Enter a number amongs {prime_numbs} : "))
 
-    max_num = max(nums)
-    primes = sieve_of_eratosthenes(max_num)
+                original_set.remove(player_input)
+                original_set -= {i for i in range(player_input*2, max(original_set)+1, player_input)}
+                mariaWins += 1
+                player_turn = 'Ben'
+            else:
+                prime_numbs = sieve_of_eratosthenes(max(original_set))
+                player_input = int(input(f"{player_turn}'s turn. Enter a number amongs {prime_numbs} : "))
+                original_set.remove(player_input)
+                original_set -= {i for i in range(player_input*2, max(original_set)+1, player_input)}
+                benWins += 1
+                player_turn = 'Maria'
 
-    maria_wins = sum(1 for n in nums if find_winner(primes, n) == 'Maria')
-    ben_wins = x - maria_wins
-
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
+    if mariaWins > benWins:
+        return 'maria'
+    elif benWins > mariaWins:
         return 'Ben'
     else:
         return None
+
+
+                
+
+print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))                  
